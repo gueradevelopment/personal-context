663ac38c90229ed53977df74c4fbd3114a3e4e55
geraJuarez <geracjuarez@gmail.com>
2019-05-12 19:00:59 -0500

    Add rabbit to all endpoints
    Needs testing

--- models/board.go
+++ models/board.go
@@ -2,7 +2,9 @@ package models
 
 // Board model
 type Board struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	GuerabookID string `json:"guerabookId"`
+	ID            string `json:"id,omitempty"`
+	Title         string `json:"title"`
+	UserID        string `json:"userId"`
+	IsTeamContext bool   `json:"isTeamContext"`
+	GuerabookID   string `json:"guerabookId,omitempty"`
 }
