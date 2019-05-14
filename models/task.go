7885f3a524a0c6ad26155bee429e2f5906ef4d7d
geraJuarez <geracjuarez@gmail.com>
2019-05-14 16:32:18 -0500

    omit some attributes in the model

--- models/task.go
+++ models/task.go
@@ -4,10 +4,10 @@ package models
 type Task struct {
 	ID              string `json:"id,omitempty"`
 	Title           string `json:"title"`
-	Description     string `json:"description"`
+	Description     string `json:"description,omitempty"`
 	UserID          string `json:"userId"`
-	IsTeamContext   bool   `json:"isTeamContext"`
-	CompletionState string `json:"completionState"`
+	IsTeamContext   bool   `json:"isTeamContext,omitempty"`
+	CompletionState string `json:"completionState,omitempty"`
 	CompletionDate  string `json:"completionDate,omitempty"`
 	BoardID         string `json:"boardId,omitempty"`
 	ChecklistID     string `json:"checklistId,omitempty"`
