eaebc524a6c368d6bdce47c393b0406500470c43
geraJuarez <geracjuarez@gmail.com>
2019-05-14 15:23:29 -0500

    fix cheklist model for those with no descriptions

--- models/checklist.go
+++ models/checklist.go
@@ -2,11 +2,12 @@ package models
 
 // Checklist model
 type Checklist struct {
-	ID             string `json:"id,omitempty"`
-	Title          string `json:"title"`
-	Description    string `json:"description"`
-	UserID         string `json:"userId"`
-	IsTeamContext  bool   `json:"isTeamContext"`
-	CompletionDate string `json:"completionDate,omitempty"`
-	BoardID        string `json:"boardId,omitempty"`
+	ID              string `json:"id,omitempty"`
+	Title           string `json:"title"`
+	Description     string `json:"description,omitempty"`
+	UserID          string `json:"userId"`
+	IsTeamContext   bool   `json:"isTeamContext"`
+	CompletionState bool   `json:"completionState,omitempty"`
+	CompletionDate  string `json:"completionDate,omitempty"`
+	BoardID         string `json:"boardId,omitempty"`
 }
